76cfa33527787c66337a91377e9d70329107126a
Michelangelo Partipilo <michelangelo.partipilo@gmail.com>
2018-07-11 10:50:22 +0200

    Trying to get javascript to work

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -14,13 +14,38 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
+        {
+          name: "description",
+          content: "==mdescr=="
+        },
+        { name: "keywords", content: "==mkeyw==" },
+        { name: "http-equiv", content: "" },
+        { name: "author", content: "==mauthor==" },
+        { name: "HandheldFriendly", content: "True" },
+        { name: "MobileOptimized", content: "320" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
+        },
+        { "http-equiv": "cleartype", content: "on" }
       ]}
       bodyAttributes={{
         class: "home"
       }}
-    />
+    >
+      /* -- ==ex_meta_taga== -- */
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <script type="text/javascript">
+        {`
+          document.cookie='resolution='+Math.max(screen.width,screen.height)+';
+          path=/';
+        `}
+      </script>
+      <script src="/assets/js/libs/modernizr-2.8.3.min.js" />
+      <script src="/assets/js/libs/vendor.js" type="text/javascript" />
+      <script async src="/assets/js/main.js" onLoad="app.init()" />
+    </Helmet>
     <Header
       siteTitle={data.site.siteMetadata.title}
       languages={data.site.siteMetadata.languages}
@@ -107,10 +132,7 @@ const Layout = ({ children, data }) => (
         </div>
       </footer>
     </div>
-    ==video== ==jsVars== ==scripts-load-top==
-    <script src="/assets/js/libs/vendor.js" />
-    <script async src="/assets/js/main.js" onLoad="app.init()" />
-    ==slideshow-script== ==analytics==
+    ==video== ==jsVars== ==scripts-load-top== ==slideshow-script== ==analytics==
   </React.Fragment>
 )
 
