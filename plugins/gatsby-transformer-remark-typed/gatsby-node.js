125d4aa77d027391caed1cfd2f2eba55088337ed
Michelangelo Partipilo <michelangelo@zirconia.local>
2018-10-16 13:28:32 +0200

    Updated markdown plugin

---
+++ plugins/gatsby-transformer-remark-typed/gatsby-node.js
@@ -0,0 +1,38 @@
+const crypto = require(`crypto`)
+const Case = require(`case`)
+
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNode, createParentChildLink } = boundActionCreators
+
+  // We only care about MarkdownRemark content.
+  if (node.internal.type !== "MarkdownRemark") {
+    return
+  }
+
+  const fileNode = getNode(node.parent)
+
+  const { sourceInstanceName } = fileNode
+  const soureceInstanceNameNormal = Case.pascal(sourceInstanceName)
+
+  const markdownNode = {
+    id: `${node.id}${soureceInstanceNameNormal}`,
+    children: [node.id],
+    parent: fileNode.id,
+    internal: {
+      type: `MarkdownRemark${soureceInstanceNameNormal}`
+    }
+  }
+
+  markdownNode.frontmatter = {
+    ...node.frontmatter,
+    _PARENT: fileNode.id
+  }
+
+  markdownNode.internal.contentDigest = crypto
+    .createHash(`md5`)
+    .update(JSON.stringify(markdownNode.frontmatter))
+    .digest(`hex`)
+
+  createNode(markdownNode)
+  createParentChildLink({ parent: markdownNode, child: node })
+}
