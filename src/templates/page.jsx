abf792686d68f62f677533d884c77863cc3decf9
Michelangelo Partipilo <michelangelo@zirconia.local>
2018-10-17 10:34:43 +0200

    Moved some common code

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -7,7 +7,7 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import Slideshow from "../components/slideshow"
 
-import contentData from "../components/i18n-data"
+import { getSlideshowData, contentData } from "../components/i18n-data"
 
 const GeneralPage = ({
   location,
@@ -53,57 +53,6 @@ const GeneralPage = ({
     </div>
   </React.Fragment>
 )
-
-function createSlide(m) {
-  var src = m.imgslide_path
-  var cap = m.imgslide_caption
-  var cap_heading = m.caption_heading
-  //var alt = m.imgslide_alt
-  var button = m.button_label
-  var button_url = m.button_url
-  var youtube_id = m.youtube_id
-  var id = m.imgslide_id
-
-  var button_view = ""
-
-  if (button && button_url) {
-    button_view = `<div><a class="btn" href="${button_url}">${button}</a></div>`
-  }
-
-  var video_button = ""
-  var video_html = ""
-  if (youtube_id) {
-    video_button = `<div><a href="#" data-href="#slide-${id}" class="btn video-link"><i class="fa fa-youtube-play"></i>watch video</a></div>`
-    video_html = `<div class="video-wrap" id="slide-${id}"><span>loading video...</span><div class="text-right"><i class="fa fa-times"></i></div><iframe width="100%" height="95%" data-src="https://www.youtube.com/embed/${youtube_id}?rel=0&autoplay=1&showinfo=1" frameborder="0" allowfullscreen></iframe></div>`
-  }
-
-  var capHTML = ""
-  if (cap_heading) {
-    if (youtube_id) {
-      capHTML = `<span>${cap_heading}</span><span class="caption">${cap}</span>${video_button}`
-    } else {
-      capHTML = `<span>${cap_heading}</span><span class="caption">${cap}</span>${button_view}`
-    }
-  }
-
-  return {
-    slide: { image: src, title: capHTML },
-    video_html
-  }
-}
-
-function getSlideshowData(imagesSlides, groupId) {
-  var slides = imagesSlides
-    .filter(f => f.imggrp_id == groupId)
-    .sort((a, b) => a.rank - b.rank)
-
-  var slideData = slides.map(createSlide)
-
-  return {
-    slides: slideData.map(s => s.slide),
-    videos_html: slideData.map(s => s.video_html).join("\r\n")
-  }
-}
 
 const GeneralPageTemplate = props => {
   const { location, data, pathContext } = props
