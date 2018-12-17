e6bd4b3caa8537526e483638fb27c0a7d1892329
Michelangelo Partipilo <michelangelo.partipilo@gmail.com>
2018-12-17 14:59:06 +0100

    Updated whatshelp widget

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -10,13 +10,11 @@ class Layout extends React.Component {
   componentDidMount() {
     var options = {
       facebook: "290339974371704", // Facebook page ID
-      whatsapp: "+995 570 70 72 14", // WhatsApp number
+      whatsapp: "+31627468794", // WhatsApp number
       email: "info@tikatours.com", // Email
       call: "+995 570 70 72 14", // Call phone number
-      company_logo_url:
-        "//storage.whatshelp.io/widget/54/54e8/54e841405d2a73f7a8d3103339540050/logo.jpg", // URL of company logo (png, jpg, gif)
-      greeting_message:
-        "Hello, how may we assist you? Send us a message and our team will be in touch shortly.", // Text of greeting message
+      company_logo_url: "//storage.whatshelp.io/widget/a5/a50c/a50c13510f5f1806f76bc4fabcb3d87c/logo.jpg", // URL of company logo (png, jpg, gif)
+      greeting_message: "Hello, how may we assist you? Send us a message and our team will be in touch shortly.", // Text of greeting message
       call_to_action: "Message or Call Us", // Call to action
       button_color: "#B21F28", // Color of button
       position: "right", // Position may be 'right' or 'left'
@@ -29,8 +27,8 @@ class Layout extends React.Component {
       shift_vertical: 0, // Vertical position, px
       shift_horizontal: 0, // Horizontal position, px
       domain: "tikatours.com", // site domain
-      key: "JV24bhnzT7uNGCGueeRdiA" // pro-widget key
-    }
+      key: "JV24bhnzT7uNGCGueeRdiA", // pro-widget key
+  };
 
     var proto = document.location.protocol,
       host = "whatshelp.io",
@@ -50,10 +48,6 @@ class Layout extends React.Component {
     const { children } = this.props
     return children()
   }
-}
-
-Layout.propTypes = {
-  children: PropTypes.Function
 }
 
 export default Layout
