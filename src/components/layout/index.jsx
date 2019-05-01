8ec8201b4abf960df089ec4b1da0b756d40281a5
Michelangelo Partipilo <michelangelo.partipilo@gmail.com>
2019-05-01 17:48:37 +0200

    Replaced WhatsHelp component by plugin

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -50,31 +50,6 @@ class Layout extends React.Component {
         />
         {children}
         <Footer {...{contact_data, language, strings}} />
-        <WhatsHelp
-          options={{
-            facebook: "290339974371704", // Facebook page ID
-            whatsapp: "+31627468794", // WhatsApp number
-            email: "info@tikatours.com", // Email
-            call: "+995 570 70 72 14", // Call phone number
-            company_logo_url:
-              "//storage.whatshelp.io/widget/a5/a50c/a50c13510f5f1806f76bc4fabcb3d87c/logo.jpg", // URL of company logo (png, jpg, gif)
-            greeting_message:
-              "Hello, how may we assist you? Send us a message and our team will be in touch shortly.", // Text of greeting message
-            call_to_action: "Message or Call Us", // Call to action
-            button_color: "#B21F28", // Color of button
-            position: "right", // Position may be 'right' or 'left'
-            order: "facebook,whatsapp,call,email", // Order of buttons
-            ga: true, // Google Analytics enabled
-            branding: false, // Show branding string
-            mobile: true, // Mobile version enabled
-            desktop: true, // Desktop version enabled
-            greeting: true, // Greeting message enabled
-            shift_vertical: 0, // Vertical position, px
-            shift_horizontal: 0, // Horizontal position, px
-            domain: "tikatours.com", // site domain
-            key: "JV24bhnzT7uNGCGueeRdiA" // pro-widget key
-          }}
-        />
       </>
     )
   }
